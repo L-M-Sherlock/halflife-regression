016e5374fba728419fa6ebb4b30d81e3aa67108c
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-12-14 22:07:56 +0800

    增加新的数据评估统计

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import math
 from sklearn.metrics import roc_curve, auc
@@ -25,30 +26,85 @@ def log_metric(l1, l2):
                  range(len(l1))])
 
 
-df = pd.read_csv("results/test.csv", sep=',')
-df['p'] = df['p'].map(lambda x: round(x))
+def load_brier(predictions, real, bins=20):
+    counts = np.zeros(bins)
+    correct = np.zeros(bins)
+    prediction = np.zeros(bins)
+    for p, r in zip(predictions, real):
+        bin = min(int(p * bins), bins - 1)
+        counts[bin] += 1
+        correct[bin] += r
+        prediction[bin] += p
+    prediction_means = prediction / counts
+    prediction_means[np.isnan(prediction_means)] = ((np.arange(bins) + 0.5) / bins)[np.isnan(prediction_means)]
+    correct_means = correct / counts
+    # correct_means[np.isnan(correct_means)] = 0
+    size = len(predictions)
+    answer_mean = sum(correct) / size
+    return {
+        "reliability": sum(counts * (correct_means - prediction_means) ** 2) / size,
+        "resolution": sum(counts * (correct_means - answer_mean) ** 2) / size,
+        "uncertainty": answer_mean * (1 - answer_mean),
+        "detail": {
+            "bin_count": bins,
+            "bin_counts": list(counts),
+            "bin_prediction_means": list(prediction_means),
+            "bin_correct_means": list(correct_means),
+        }
+    }
 
-print("%%%%%%%%%%%%%%%% ROC Curve %%%%%%%%%%%%%%%%")
 
-fpr, tpr, threshold = roc_curve(df['p'], df['pp'])
-roc_auc = auc(fpr, tpr)
-print('AUC:', roc_auc)
+def plot_brier(predictions, real, bins=20):
+    brier = load_brier(predictions, real, bins=bins)
+    plt.figure()
+    plt.plot((0, 1), (0, 1), label='Optimal average observation')
+    plt.plot(brier['detail']['bin_prediction_means'], brier['detail']['bin_correct_means'],  '*', label='Average observation',)
+    bin_count = brier['detail']['bin_count']
+    counts = np.array(brier['detail']['bin_counts'])
+    bins = (np.arange(bin_count) + 0.5) / bin_count
+    plt.legend(loc='upper center')
+    plt.xlabel('Prediction')
+    plt.ylabel('Observeation')
+    plt.twinx()
+    plt.ylabel('Number of predictions')
+    plt.bar(bins, counts, width=(0.5 / bin_count), alpha=0.5, label='Number of predictions')
+    plt.legend(loc='lower center')
 
-plt.figure()
-lw = 2
-plt.figure(figsize=(10, 10))
-plt.plot(fpr, tpr, color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  # 假正率为横坐标，真正率为纵坐标做曲线
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic example')
-plt.legend(loc="lower right")
-plt.show()
+if __name__ == "__main__":
+    # df = pd.read_csv("con_test_1214-2.csv", sep=',')
+    # df = pd.read_csv('results/lr.con_test_1214-2.preds', sep='\t')
+    # df = pd.read_csv('results/lr.cr.con_test.preds', sep='\t')
+    # df = pd.read_csv('results/anki.con_test.preds', sep='\t')
+    # df = pd.read_csv('results/leitner.con_test.preds', sep='\t')
+    # df = pd.read_csv('results/hlr.cr.con_test.preds', sep='\t')
+    df = pd.read_csv('results/test.csv', sep=',')
+    df['p'] = df['p'].map(lambda x: round(x))
 
-print("%%%%%%%%%%%%%%%% metric %%%%%%%%%%%%%%%%")
-print('mean average error:', mae(df['p'], df['pp']))
-print('root mean squared error:', rmse(df['p'], df['pp']))
-print('log metric:', log_metric(df['p'], df['pp']))
+    plot_brier(df['pp'], df['p'], 100)
+    plt.show()
+
+    print("%%%%%%%%%%%%%%%% ROC Curve %%%%%%%%%%%%%%%%")
+
+    fpr, tpr, threshold = roc_curve(df['p'], df['pp'])
+    roc_auc = auc(fpr, tpr)
+    print('AUC:', roc_auc)
+
+    plt.figure()
+    lw = 2
+    plt.figure(figsize=(10, 10))
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  # 假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+    print("%%%%%%%%%%%%%%%% metric %%%%%%%%%%%%%%%%")
+    # print('mean average error:', mae(df['p'], df['pp']))
+    print('root mean squared error:', rmse(df['p'], df['pp']))
+    print('log metric:', log_metric(df['p'], df['pp']))
